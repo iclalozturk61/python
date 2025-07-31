dc97eab50e9883fac57060e96ceb4c5207057255
iclalozturk61 <iclalozturk@ogr.iuc.edu.tr>
2025-07-31 12:08:30 +0300

    to return with value loaded

--- caleb-curry-beginner/10-functions.py
+++ caleb-curry-beginner/10-functions.py
@@ -14,7 +14,38 @@ greet()
 greet()
 
 #Arguments and Parameters YARIM KALDI
-def greet(name): #bunun adı parameters ()
-    print("merhabalar" , name)
+def greet(name): #bunun adı parameters fonksyonun aldığı şeydir (parameters are the variables on the inside of the function)
+    print("merhabalar" , name , "argüman örneğidir")
 
-greet("iclal") #bunun adı arguments ()
+greet("iclal") #bunun adı arguments fonksyona verdiğimiz şeydir (arguments you pass the things)
+
+#return break gibi ama o döngüler için kullanılır bunu fonksyonlar için kullanılır. Fonksyonun içinde döngü varsa break ve continue kullanılabilir ama direk fonksyon içnde kullanılamaz
+def greet(name):
+    if name == "fellas":
+        print("1. return çıktısı")
+        return 
+    print("fonksyonun içindeki asla okunmayacak şeyler")
+    
+greet("fellas")
+
+#return vs else to Exit Function: ikisi de olur ama okunabilirlik ve basitlik gereği return bence daha iyi
+def greet(name):
+    if name == "fellas":
+        print("1. else ile çıkıldı")
+     
+    else: 
+        print("fonksyonun içindeki asla okunmayacak şeyler")
+    
+greet("fellas")
+
+#NOT: aynı dosya içinde aynı isimle iki fonksyon üretilirse python öncekileri geçersiz kılar sadece sonuncuyu önemser
+
+#How to Return a Value from Function
+def greet_2(name):
+    if name == "hatice":
+        return "oo nabıyon lan " + name #bu şekilde returne değer verilir
+    return "merhabalar greet_2 çıktısı " + name 
+
+returned = greet_2("iclal") #bu şekilde kullanılır
+print(returned)
+print(greet_2("hatice"))
